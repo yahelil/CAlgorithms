92c4a7a5a1b9750cfb91f116e24eda32b99a20a6
yahelil <117468179+yahelil@users.noreply.github.com>
2024-09-26 16:40:32 +0300

    Adding a separate function to print the suffix and correcting the algorithm

--- Maman11/Q2/my_suffix.c
+++ Maman11/Q2/my_suffix.c
@@ -2,41 +2,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#define STRING_LENGTH 127
 
 int suffix(char str[], char c){
-    char ending[126] = "";
     bool read = false;
-    int added_char = 0;
-    int len = strlen(str);
-    int i = 0;
+    int suffix = 0, len = strlen(str), i = 0;
     while (str[i]){
-        if (read){
-            strncat(ending, &str[i], 1);
-            added_char += 1;
-        }
         if (c == str[i]){
             read = true;
-            printf("%s\n", &str[i]);
-            strcpy(ending, "");  // Reset ending
-            added_char = 0;
         }
+        if (read){
+            print_ending(&str[i]);
+            read = false;
+            suffix += 1;
+
+        }
+
         i++;
     }
-    return added_char;
-
+    return suffix;
 }
+
+void print_ending(char str[]){
+    printf("%s\n", str);
+}
+
 int main()
 {
     char c;
-    char str[126];
+    char str[STRING_LENGTH];
     int num;
     printf("Enter a char: ");
     scanf(" %c", &c);
     printf("Enter a string: ");
     scanf("%s", &str);
-    printf("The character is %c and the string is %s", c, str);
+    printf("The character is %c and the string is %s\n", c, str);
     num = suffix(str, c);
-    printf("Number of characters in the suffix: %d\n", num);
+    printf("Number of suffixes in the string is %d\n", num);
     return 0;
 }
 
