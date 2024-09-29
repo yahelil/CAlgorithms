32b13015bb3b24a4c164d1f68b002115fd9bf0c8
yahelil <117468179+yahelil@users.noreply.github.com>
2024-09-29 23:19:35 +0300

    Update my_suffix.c after review

--- Maman11/Q2/my_suffix.c
+++ Maman11/Q2/my_suffix.c
@@ -1,44 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#define STRING_LENGTH 127
+#define STRING_LENGTH 128
+
 
 int suffix(char str[], char c){
-    bool read = false;
-    int suffix = 0, len = strlen(str), i = 0;
-    while (str[i]){
-        if (c == str[i]){
-            read = true;
-        }
-        if (read){
-            print_ending(&str[i]);
-            read = false;
-            suffix += 1;
+    /*
+    This function finds suffix in a string
+    When the function finds the suffix it prints it and counts it
 
-        }
+    The function iterates over the string using a while loop.
 
-        i++;
+    param str: (char[]) the string to iterate over
+    param c: (char) the char that start the suffix.
+
+    @return: (int) the number of suffix in the string.
+    if a suffix is found the program prints it.
+    */
+    int counter = 0;
+    char* currentchar;
+    for (currentchar = str; currentchar != '\0'; currentchar++){
+        if (*currentchar == 'c'){
+            printf("%s\n", currentchar);
+            counter += 1;
+        }
     }
-    return suffix;
-}
-
-void print_ending(char str[]){
-    printf("%s\n", str);
+    return counter;
 }
 
 int main()
 {
-    char c;
-    char str[STRING_LENGTH];
-    int num;
+    char c, str[STRING_LENGTH];
+    int number_of_suffix;
+    /*Asking the user for a character that starts the suffix and reciving it*/
     printf("Enter a char: ");
     scanf(" %c", &c);
+    /*Asking the user for a string to find suffix in it and reciving it*/
     printf("Enter a string: ");
     scanf("%s", &str);
-    printf("The character is %c and the string is %s\n", c, str);
-    num = suffix(str, c);
-    printf("Number of suffixes in the string is %d\n", num);
+
+    /* printing the input from the user*/
+    printf("The character is %c and the string is %s\n", c, str); 
+    number_of_suffix = suffix(str, c);
+    printf("Number of suffixes in the string is %d\n", number_of_suffix);
     return 0;
 }
 
